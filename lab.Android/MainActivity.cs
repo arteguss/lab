2a9b189b584215e0e5b877db21d3560cabaed43b
Артём Гусев <goosev.job@ya.ru>
2019-05-12 23:27:23 +0300

    First
    First commit

---
+++ lab.Android/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Microsoft.WindowsAzure.MobileServices;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace lab.Droid
+{
+    [Activity(Label = "lab", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        public static MobileServiceClient MobileService =
+    new MobileServiceClient("https://gooslab.azurewebsites.net");
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
